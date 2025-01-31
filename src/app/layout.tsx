baede4fe06a50b60ba271adc4c6730f39e49a658
mxstxchx <rafavasquezf@gmail.com>
2025-01-31 04:14:15 +0100

    Update root layout with error boundary

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider 
+          attribute="class" 
+          defaultTheme="system" 
+          enableSystem
+        >
           <SessionErrorBoundary>
             {children}
           </SessionErrorBoundary>
