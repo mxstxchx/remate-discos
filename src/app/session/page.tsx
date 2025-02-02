b0d4bb5316d547388607258879cb7741d5e136a2
mxstxchx <rafavasquezf@gmail.com>
2025-02-02 05:06:15 +0100

    Move session page from WebApp

---
+++ src/app/session/page.tsx
@@ -0,0 +1,9 @@
+export default function SessionPage() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center p-24">
+      <header className="mb-8 text-center">
+        <h1 className="text-4xl font-bold mb-4">Remate Discos</h1>
+      </header>
+    </main>
+  );
+}
